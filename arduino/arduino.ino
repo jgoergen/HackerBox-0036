f181aba4dc6dfd5f623d16112176d6b0a13729ef
Jeff Goergen <danthestickman@yahoo.com>
2019-04-11 08:39:11 -0400

    updates

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,8 @@
 // Core display library provided by the PxMatrix library https://github.com/2dom/PxMatrix
 // Basic integration provided by Hackerboxes https://www.instructables.com/id/HackerBox-0036-JumboTron/
 #include <PxMatrix.h>
+#include <Wire.h>
+#include <MPU9250_asukiaaa.h>
 
 // ################# SETTINGS ###########################################
 
@@ -10,6 +12,7 @@
 #define INPUT_WAIT          100 // ~10 fps
 #define DISPLAY_WAIT        33 // ~30 fps
 #define VERLET_WAIT         10 // ~100 fps
+#define MPU_WAIT            50 // ~20 fps
 #define VERLET_PRECISION    100
 
 #define MIN_BALLS_IN_SCENE  100
@@ -19,7 +22,7 @@
 
 // ################ PINS ################################################
 
-// ESP32 Pins for JoyStick
+// Pins for JoyStick
 #define JOY_X 35
 #define JOY_Y 34
 #define JOY_Z 26
@@ -28,7 +31,7 @@
 #define KEY_3 33
 #define KEY_4 25
 
-// ESP32 Pins for LED MATRIX
+// Pins for LED MATRIX
 #define P_LAT 4
 #define P_A 19
 #define P_B 23
@@ -37,16 +40,26 @@
 #define P_E 15  // NOT USED for 1/16 scan
 #define P_OE 2
 
+// Pins for the MPU9250
+#define SDA_PIN 21
+#define SCL_PIN 22
+
 // ################ MISC ################################################
 
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 PxMATRIX display(64,32,P_LAT, P_OE,P_A,P_B,P_C,P_D);
+MPU9250 mySensor;
+
 uint8_t display_draw_time=0;
 float ax, ay, az;
 int lastInput = 0;
 int lastVerlet = 0;
 int lastDisplay = 0;
+int lastMPU = 0;
+int magX = 0;
+int magY = 0;
+int magZ = 0;
 
 // ################################# SETUP ###################################
 
@@ -62,6 +75,10 @@ void setup() {
   pinMode(KEY_2, INPUT_PULLUP);
   pinMode(KEY_3, INPUT_PULLUP);
   pinMode(KEY_4, INPUT_PULLUP);
+
+  Wire.begin(SDA_PIN, SCL_PIN);
+  mySensor.setWire(&Wire);
+  mySensor.beginMag();
 
   display_init();
   verlet_init();
@@ -88,7 +105,7 @@ void loop() {
 
     if (digitalRead(KEY_4) == LOW)
       verlet_removeBall();    
-
+/*
     // translate joystick x/y into gravity between -0.25 and 0.25, reverse y as well.
     float x = (0.025f - (0.05f * ((float)analogRead(JOY_X) / 4096.0f))) * -1;
     float y = (0.025f - (0.05f * ((float)analogRead(JOY_Y) / 4096.0f)));
@@ -98,6 +115,31 @@ void loop() {
     if (y < 0.01f && y > -0.01f) y = IDLE_GRAVITY_Y;
       
     verlet_updateGravity(x, y);
+    */
+  }
+
+  // update verlet integration
+  if (millis() - lastMPU > MPU_WAIT) {
+    
+    lastMPU = millis();
+    mySensor.magUpdate();
+    magX = mySensor.magX();
+    magY = mySensor.magY();
+    magZ = mySensor.magZ();
+
+    // x = left / right tilt. center = -90, full right = 40, full left = -130. span = 170
+    // y = front / back tilt. center = 40, full forward = -40, full back = 120. span = 160
+    // z = rotation
+
+    float fixedX = 0.05f - (0.1f * ((float(magX) + 130.0f) / 170.0f));
+    float fixedY = 0.05f - (0.1f * ((float(magY) + 40.0f) / 160.0f));
+    /*Serial.println("X: " + String(fixedX) + " " + String(magX));
+    Serial.println("Y: " + String(fixedY) + " " + String(magY));
+    Serial.println("Z: " + String(magZ));
+    Serial.println(" ");
+    */
+
+    verlet_updateGravity(fixedX, fixedY);
   }
   
   // update verlet integration
